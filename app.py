a17dc7035d7b1020464165ccdddb9a49042dc976
Andrew Xie <andrewxieax1@gmail.com>
2021-09-17 22:15:19 -0700

    twilio vidchat route

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, abort
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+load_dotenv()
+twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
+twilio_api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
+
+@app.route('/video')
+def index():
+    return render_template('vidchat.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.get_json(force=True).get('username')
+    if not username:
+        abort(401)
+
+    token = AccessToken(twilio_account_sid, twilio_api_key_sid,
+                        twilio_api_key_secret, identity=username)
+    token.add_grant(VideoGrant(room='My Room'))
+
+    return {'token': token.to_jwt().decode()}
+
+if __name__ == "__main__":
+    app.run(debug=True)
