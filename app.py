2f86c23db3d8fd5e266bef06f07bd756020c4cad
annieliu10 <annieyt8880@gmail.com>
2021-09-17 22:09:23 -0700

    create the post and put endpoints to login and edit the profile information

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 profile = {
     'name': "Annie Liu",
-    'age': "20",
+    'age': 20,
     'address': "2205 Lower Mall",
     'emergency contact': "Linda Ma",
     'allergies': [],
@@ -24,4 +24,49 @@ def profile():
 def retrieve():
     return jsonify({'profile': profile})
 
+@app.route('/signup', methods=["POST"])
+def signup():
+    if not all(userField in request.json for userField in ("name", "age", "address", "emergency contact", "blood type", "BMI", "height", "weight")):
+        abort(400, description="Resource not found")
+    if not (field is str for field in ("name", "address", "emergency contact", "blood type")):
+        abort(400, description="Keys are supposed to be string")
+    if not (field is int or float for field in ("age", "BMI", "weight", "height")):
+        abort(400, description="Keys are supposed to be numbers")
+        
+    profile['name'] = request.json['name']
+    profile['age'] = request.json['age']
+    profile['address'] = request.json['address']
+    profile['emergency contact'] = request.json['emergency contact']
+    profile['allergies'] = request.json['allergies']
+    profile['blood type'] = request.json['blood type']
+    profile['conditions'] = request.json['conditions']
+    profile['medications'] = request.json['medications']
+    profile['BMI'] = request.json['BMI']
+    profile['height'] = request.json['height']
+    profile['weight'] = request.json['weight']
+
+    return jsonify(result = "Account created!")
+
+@app.route('/edit', methods=['PUT'])
+def edit():
+    if not all(userField in request.json for userField in ("name", "age", "address", "emergency contact", "blood type", "BMI", "height", "weight")):
+        abort(400, description="Resource not found")
+    if not (field is str for field in ("name", "address", "emergency contact", "blood type")):
+        abort(400, description="Keys are supposed to be string")
+    if not (field is int or float for field in ("age", "BMI", "weight", "height")):
+        abort(400, description="Keys are supposed to be numbers")
+
+    profile['name'] = request.json.get(['name'], profile['name'])
+    profile['age'] = request.json.get(['age'], profile['age'])
+    profile['address'] = request.json.get(['address'],profile['address'])
+    profile['emergency contact'] = request.json.get(['emergency contact'],profile['emergency contact'])
+    profile['allergies'] = request.json.get(['allergies'],  profile['allergies'])
+    profile['blood type'] = request.json.get(['blood type'], profile['blood type'])
+    profile['conditions'] = request.json.get(['conditions'], profile['conditions'] )
+    profile['medications'] = request.json.get(['medications'], profile['medications'])
+    profile['BMI'] = request.json.get(['BMI'], profile['BMI'])
+    profile['height'] = request.json.get(['height'], profile['height'])
+    profile['weight'] = request.json.get(['weight'], profile['weight'])
+    
+    return jsonify({'profile': profile}, result = "Successfully edited!")
 
